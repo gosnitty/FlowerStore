3a802f9e9126152528829a009da9b20b701b5b7f
gosnitty <116552150+gosnitty@users.noreply.github.com>
2023-10-12 21:00:13 +0300

    Add files via upload

---
+++ FlowerStore/src/main/java/flower/store/FlowerPack.java
@@ -0,0 +1,17 @@
+package flower.store;
+
+public class FlowerPack {
+
+    private Flower flower;
+    private int quantity;
+
+    public FlowerPack(Flower flower, int quantity) {
+        this.flower = flower;
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return flower.getPrice()*quantity;
+    }
+
+}
